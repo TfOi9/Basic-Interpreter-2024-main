3c4ea4692d7ca3c6ecbbfbd0fd1a6f825d7ea523
TfOi9 <2165542363@qq.com>
2025-08-02 13:36:33 +0800

    Add files via upload

---
+++ Utils/error.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include "error.hpp"
+
+ErrorException::ErrorException(std::string message) {
+    this->message = message;
+}
+
+ErrorException::~ErrorException() throw() = default;
+
+std::string ErrorException::getMessage() const {
+    return message;
+}
+
+//----------------------------------------------------------------------------------------
+
+void error(std::string message) {
+    throw ErrorException(message);
+}
